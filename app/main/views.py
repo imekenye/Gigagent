acb1763465791c2f7b34e2ae9870410b7ff59175
gichimux <gichimumwai1@gmail.com>
2019-05-02 08:31:17 +0300

    created a global search form object

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,8 @@ from . import main
 from .. import db
 from flask_login import login_required
 from ..models import User
+from forms import ArtistSearchForm
+
 @main.route('/')
 def index():
     '''
@@ -12,3 +14,13 @@ def index():
     title = 'Home - Welcome to Gigagent'
 
     return render_template('index.html', title=title)
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+        g.search_form = ArtistSearchForm()
