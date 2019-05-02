5999659e7082883adf1e07e39900187f4aa356f0
gichimux <gichimumwai1@gmail.com>
2019-05-02 08:41:15 +0300

    created view's route to search results display

--- app/main/views.py
+++ app/main/views.py
@@ -31,3 +31,10 @@ def search():
     if not g.search_form.validate_on_submit():
         return redirect(url_for('index'))
     return redirect(url_for('search_results', query=g.search_form.search.data))
+
+
+@app.route('/search_results/<query>')
+@login_required
+def search_results(query):
+    results = Post.query.whoosh_search(query, MAX_SEARCH_RESULTS).all()
+    return render_template('search_results.html', query=query, results=results)
